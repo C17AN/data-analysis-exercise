869a25337920726c8b6f5d261876cbc84ec8545d
chanmin <kimchanmin1@gmail.com>
2025-03-02 12:21:43 +0900

    feat: R 폴더를 추가한다

---
+++ practices/R/01.r
@@ -0,0 +1,15 @@
+a <- c(1,2,3)
+b <- c(4,5,6)
+c <- c(a*b)
+d <- as.factor(1:4)
+e <- as.character(1:4)
+f <- c("1", "hi", "hoho")
+g <- c(1, 2, 3)
+i <- 3
+h <- i > 2
+n <- rep(10, 5)
+o <- 1:5
+p <- cbind(n, o)
+q <- rbind(n, o)
+r <- matrix(1:8, 2, 4)
+s <- matrix(c(1,4,2,7), 2,2)
